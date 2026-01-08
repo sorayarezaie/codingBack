8f4a524b5b625dd591828f5be9fb5d57578949e5
sorayarezaie <banorezaie2004@gmail.com>
2026-01-08 19:44:09 +0430

    id added

--- new js/app.js
+++ new js/app.js
@@ -2,7 +2,7 @@ let studentInfo = {
     name : "Ali"  , "Ahmad", "Fatima", 
     lastName : "Ahmadi" , "Hakimi", "Rahimi",
     phoneNumber : "0798464332", "0794045676", "0798431292", 
-    ID : "1" , 
+    ID : "1" , "2"
 
 }
 
