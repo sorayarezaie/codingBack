b32fe7a9f89f9df83a7ae423ac534f99d5a94a22
sorayarezaie <banorezaie2004@gmail.com>
2026-01-08 19:46:37 +0430

    new info deleted

--- new js/app.js
+++ new js/app.js
@@ -1,8 +1,8 @@
 let studentInfo = {
-    name : "Ali"  , "Ahmad", "Fatima", 
-    lastName : "Ahmadi" , "Hakimi", "Rahimi",
-    phoneNumber : "0798464332", "0794045676", "0798431292", 
-    ID : "1" , "2", "3"
+    name : "Ali"  , 
+    lastName : "Ahmadi" ,
+    phoneNumber : "0798464332",
+    ID : "1" ,
 
 }
 
